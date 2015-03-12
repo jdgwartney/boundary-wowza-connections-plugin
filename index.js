f830e6ccd703b797da22d8ba9f5b58a279bd4e56
Keith Janak <kjjanak@gmail.com>
2015-03-12 13:26:39 -0500

    node test

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-Test
+var fs = require('fs'),
+    xml2js = require('xml2js');
+
+var parser = new xml2js.Parser();
+fs.readFile(__dirname + '/test.xml', function(err, data) {
+    parser.parseString(data, function (err, result) {
+        console.log(result, false, null);
+    });
+});
